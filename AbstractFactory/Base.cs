55c4cee72da915191c64d9926c0d4e6aaac207bf
LeninRevan <lenintanooki@gmail.com>
2026-05-08 00:14:58 -0600

    El codigo implementado para un ejemplo

---
+++ AbstractFactory/Base.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
+{
+    // AbstractFactory declara métodos para la creación de productos 
+    public interface IAbstractFactory
+    {
+        IAbstractProductA CreateProductA();
+
+        IAbstractProductB CreateProductB();
+    }
+
+    // Concrete Factories implementan los métodos de creación
+    // pero devolviendo productos concretos
+    class ConcreteFactory1 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA1();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB1();
+        }
+    }
+
+    // Cada ConcreteFactory construye productos de un tipo
+    class ConcreteFactory2 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA2();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB2();
+        }
+    }
+
+    // Los productos base se representan como interfaces
+    public interface IAbstractProductA
+    {
+        string UsefulFunctionA();
+    }
+
+    // Los Concrete Products implementan la base 
+    class ConcreteProductA1 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A1.";
+        }
+    }
+
+    class ConcreteProductA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A2.";
+        }
+    }
+
+    // La base indica las características primordiales del producto
+    public interface IAbstractProductB
+    {
+        string UsefulFunctionB();
+
+        // En nuestro ejemplo tenemos que los productos pertenecen a una categoría definida
+        // por las fábricas concretas, pero también está
+        string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+    }
+
+    // Los ConcreteProduct implementan los métodos abstractos de sus bases
+    class ConcreteProductB1 : IAbstractProductB
+    {
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B1.";
+        }
+
+        // Aquí se ilustra la compatibilidad brindada por la relación de los objetos base
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B1 collaborating with the ({result})";
+        }
+    }
+
+    class ConcreteProductB2 : IAbstractProductB
+    {
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B2.";
+        }
+
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B2 collaborating with the ({result})";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("Client: Testing client code with the first factory type...");
+            ClientMethod(new ConcreteFactory1());
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Testing the same client code with the second factory type...");
+            ClientMethod(new ConcreteFactory2());
+        }
+
+        public void ClientMethod(IAbstractFactory factory)
+        {
+            var productA = factory.CreateProductA();
+            var productB = factory.CreateProductB();
+
+            Console.WriteLine(productB.UsefulFunctionB());
+            Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+        }
+    }
+}
